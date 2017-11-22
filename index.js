5447c0ad2b2016bc38da0e2772fbba5b5e51e123
Walker Langley <walkerlangley@gmail.com>
2017-11-21 22:26:47 -0500

    Add swp files to .gitignore

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const mysql = require('mysql')
 const graphqlHTTP = require('express-graphql')
-const { buildSchema } = require('graphql')
-const PORT = 4000;
 const schema = require('./schema/schema')
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'library'
-})
-
-connection.connect()
+const PORT = 4000;
 
 const app = express()
-//app.use(bodyParser.json())
 app.use('/graphiql', graphqlHTTP({
   schema,
   graphiql: true
@@ -25,15 +13,6 @@ app.use('/graphiql', graphqlHTTP({
 app.get('/health', (req, res) => {
   res.send('OK!')
 })
-
-app.get('/db', (req, res) => {
-  connection.query('SELECT * from users', (err, rows, fields) => {
-    if (err) res.send(err)
-    console.log('Users: ', rows[0]);
-  })
-})
-
-
 
 app.listen(PORT, () => {
   console.log(`Server Started on PORT: ${PORT}`);
